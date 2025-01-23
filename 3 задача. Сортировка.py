6fa128c7133efc6366b1193ba7ef9354f8891f5c
Kartishev <Kartishev.djek@yandex.ru>
2025-01-24 00:21:31 +0300

    Changed 3 task

--- 3 задача. Сортировка.py
+++ 3 задача. Сортировка.py
@@ -1,22 +1,25 @@
+import os
+
+list_files_names = []
+list_lists_changed_files = []
+
+for file in os.listdir("."):
+    if file.endswith(".txt"):
+        list_files_names.append(file)
+
 def read_file(file):
-    list_file = file.readlines()
-    list_file.insert(0, str(file.name) + "\n")
-    list_file.insert(1, str(len(list_file)-1) + "\n")
-    return list_file
+    list_str = file.readlines()
+    list_str.insert(0, str(file.name) + "\n")
+    list_str.insert(1, str(len(list_str)-1) + "\n")
+    return list_str
 
-with open('1.txt', encoding='utf-8') as f1:
-    list_file1 = read_file(f1)
+for file_name in list_files_names:
+    with open(file_name, encoding='utf-8') as f:
+        list_lists_changed_files.append(read_file(f))
 
-with open('2.txt', encoding='utf-8') as f2:
-    list_file2 = read_file(f2)
+list_lists_changed_files.sort(key=len)
 
-with open('3.txt', encoding='utf-8') as f3:
-    list_file3 = read_file(f3)
-
-list_all = [list_file1, list_file2, list_file3]
-list_all.sort(key=len)
-
-with open('Result.txt', 'a', encoding='utf-8') as f4:
-    for list_any in list_all[0:-1]:
-        f4.write("".join(list_any) + "\n")
-    f4.write("".join(list_all[-1]))
+with open('Result.md', 'a', encoding='utf-8') as f1:
+    for list_any in list_lists_changed_files[0:-1]:
+        f1.write("".join(list_any) + "\n")
+    f1.write("".join(list_lists_changed_files[-1]))
