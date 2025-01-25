46c985078248c139ac3ee42569a933b9afc8b384
Kartishev <Kartishev.djek@yandex.ru>
2025-01-26 02:29:30 +0300

    Changed 3 task new

--- 3 задача. Сортировка.py
+++ 3 задача. Сортировка.py
@@ -1,25 +1,24 @@
 import os
 
-list_files_names = []
-list_lists_changed_files = []
+list_changed_files = []
 
-for file in os.listdir("."):
-    if file.endswith(".txt"):
-        list_files_names.append(file)
-
-def read_file(file):
-    list_str = file.readlines()
-    list_str.insert(0, str(file.name) + "\n")
+def read_file(file_):
+    list_str = file_.readlines()
+    list_str.insert(0, str(file_.name) + "\n")
     list_str.insert(1, str(len(list_str)-1) + "\n")
     return list_str
 
-for file_name in list_files_names:
-    with open(file_name, encoding='utf-8') as f:
-        list_lists_changed_files.append(read_file(f))
+for file in os.listdir("."):
+    if file.endswith(".txt") and os.path.basename(file) != "Result.txt":
+        with open(file, encoding='utf-8') as f:
+            list_changed_files.append(read_file(f))
 
-list_lists_changed_files.sort(key=len)
+list_changed_files.sort(key=len)
 
-with open('Result.md', 'a', encoding='utf-8') as f1:
-    for list_any in list_lists_changed_files[0:-1]:
-        f1.write("".join(list_any) + "\n")
-    f1.write("".join(list_lists_changed_files[-1]))
+with open('Result.txt', 'a', encoding='utf-8') as f1:
+    if len(list_changed_files) > 1:
+        for list_any in list_changed_files[0:-1]:
+            f1.write("".join(list_any) + "\n")
+        f1.write("".join(list_changed_files[-1]))
+    elif len(list_changed_files) == 1:
+        f1.write("".join(list_changed_files[0]))
